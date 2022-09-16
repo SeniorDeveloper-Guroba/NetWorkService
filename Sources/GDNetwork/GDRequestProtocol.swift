f9366c667c612534d714098d2775572b69ba71ea
Aiub Duduev <aub.duduev.1989@gmail.com>
2022-09-16 09:11:41 +0300

    bag fixed

---
+++ Sources/GDNetwork/GDRequestProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Senior Developer on 15.09.2022.
+//
+
+import Foundation
---
+++ Sources/GDNetwork/GDSessionProtocol.swift
@@ -0,0 +1,98 @@
+//
+//  SessionProtocol.swift
+//  
+//
+//  Created by Senior Developer on 15.09.2022.
+//
+import Combine
+import Foundation
+
+public protocol GDURLSession {
+    
+    func session(url: URL?, body: Data?, httpMethod: String.HTTPMethod, header: Header?, completion: @escaping ClosureSession)
+}
+
+public extension GDURLSession {
+    
+    func session(url: URL?, body: Data? = nil, httpMethod: String.HTTPMethod, header: Header? = nil, completion: @escaping ClosureSession){
+        guard let url = url else {
+            fatalError("Error URL \(url?.absoluteString ?? "no url")")
+        }
+        var request   = URLRequest(url: url, timeoutInterval: Double.infinity)
+        request.httpMethod = httpMethod.rawValue
+        request.allHTTPHeaderFields = header
+        request.httpBody = body
+        //Request responce
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            //error return response
+            if let error = error {
+                completion(.error(error))
+                //data return response
+            } else {
+                guard let data = data else { return }
+                print(response?.url?.absoluteString as Any, "Sessionoble response URL absoluteString")
+                completion(.data(data))
+            }
+        }.resume()
+    }
+    
+    func session(url: URL?, body: Data? = nil, httpMethod: String.HTTPMethod, header: Header? = nil) throws -> AnyPublisher<Data, Error> {
+        guard let url = url else {
+            throw RequestError.responseInvalid
+        }
+        var request                 = URLRequest(url: url)
+        request.httpMethod          = httpMethod.rawValue
+        request.allHTTPHeaderFields = header
+        request.httpBody            = body
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+        
+            .tryMap { data, response -> (Data, HTTPURLResponse) in
+                if let httpResponse = response as? HTTPURLResponse {
+                    return (data, httpResponse)
+                } else {
+                    throw RequestError.responseInvalid
+                }
+            }
+            .tryMap { data, response -> Data in
+                if (200..<300).contains(response.statusCode) {
+                    return data
+                } else {
+                    throw RequestError.responseInvalid
+                }
+            }.eraseToAnyPublisher()
+    }
+    
+    @available(iOS 15.0, *)
+    func session(url: URL?, body: Data? = nil, httpMethod: String.HTTPMethod, header: Header? = nil) async throws -> Data {
+        
+        guard let url = url else {
+            throw RequestError.responseInvalid
+        }
+        var request                 = URLRequest(url: url)
+        request.httpMethod          = httpMethod.rawValue
+        request.allHTTPHeaderFields = header
+        request.httpBody            = body
+        do {
+            let (data, httpResponse) = try await URLSession.shared.data(for: request)
+            
+            if let response = httpResponse as? HTTPURLResponse, (200..<300).contains(response.statusCode) {
+                return data
+            } else {
+                throw RequestError.responseInvalid
+            }
+            
+        } catch let error {
+            throw error
+        }
+    }
+    
+    
+}
+
+enum RequestError: LocalizedError {
+    case urlInvalid
+    case requestInvalid
+    case responseInvalid
+    case internetUnavailable
+}
