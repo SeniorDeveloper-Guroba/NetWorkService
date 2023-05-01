517f18c9d816e71063e5b601f2992c9e58288a5d
Aiub Duduev <aub.duduev.1989@gmail.com>
2023-05-01 11:39:17 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -4,27 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "GDNetwork",
+    name: "RESTService",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         .library(
-            name: "GDNetwork",
-            targets: ["GDNetwork"]),
+            name: "RESTService",
+            targets: ["RESTService"]),
     ],
     dependencies: [
         .package(url: "https://github.com/ashleymills/Reachability.swift", branch: "master"),
+        .package(url: "https://github.com/SeniorDeveloper-Guroba/AlertService", branch: "master"),
     ],
     targets: [
         .target(
-            name: "GDNetwork",
+            name: "RESTService",
             dependencies: [
                 .product(name: "Reachability", package: "Reachability.swift"),
+                .product(name: "AlertService", package: "AlertService"),
                
             ]),
-        .testTarget(
-            name: "GDNetworkTests",
-            dependencies: ["GDNetwork"]),
     ]
 )
